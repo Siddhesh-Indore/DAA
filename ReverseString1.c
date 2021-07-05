ea49538d2a9d31a1156946d1d1aafcfee7bcd02c
Siddhesh Indore <siddheshindore@gmail.com>
2021-07-05 13:57:04 +0530

    Added ReverseString1.c File

---
+++ ReverseString1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    char A[] = "Python";
+    char B[7];
+    int i;int j;
+    for(i=0;A[i]!='\0';i++)
+    {}
+    i=i-1;
+    for(j=0;i>=0;i--,j++)
+    {
+        B[j]=A[i];
+    } 
+    B[j]='\0';
+    printf("%s",B);
+    return 0;
+}
